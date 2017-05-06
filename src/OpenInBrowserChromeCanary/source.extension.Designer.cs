fc233531df01e062f4a963d769a63c22b0487587
GregTrevellick <gtrevellick@googlemail.com>
2017-05-06 23:50:05 +0100

    more browser code

--- src/OpenInBrowserChromeCanary/source.extension.Designer.cs
+++ src/OpenInBrowserChromeCanary/source.extension.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OpenInGregtGregt {
+namespace OpenInChromeCanary {
     using System;
     
     
--- src/OpenInBrowserOpera/source.extension.Designer.cs
+++ src/OpenInBrowserOpera/source.extension.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OpenInGregtGregt {
+namespace OpenInOpera {
     using System;
     
     
