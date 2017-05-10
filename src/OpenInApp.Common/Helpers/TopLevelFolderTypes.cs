d32048880b30b2e9bc35dbc1ebe3664c99924fcf
GregTrevellick <gtrevellick@googlemail.com>
2017-05-10 22:14:12 +0100

    added TopLevelFolderTypes.cs

---
+++ src/OpenInApp.Common/Helpers/TopLevelFolderTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenInApp.Common.Helpers
+{
+    public enum TopLevelFolderTypes
+    {
+        ApplicationData = 26,
+        ProgramFiles = 38,
+        ProgramFilesX86 = 42,
+        Windows = 36,
+    }
+}
