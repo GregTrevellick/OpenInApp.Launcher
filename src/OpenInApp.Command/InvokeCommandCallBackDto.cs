c4dcafc405cf04437e639afa6f074f35ea52c92d
GregTrevellick <gtrevellick@googlemail.com>
2017-05-16 08:21:37 +0100

    added comments

--- src/OpenInApp.Command/InvokeCommandCallBackDto.cs
+++ src/OpenInApp.Command/InvokeCommandCallBackDto.cs
@@ -6,15 +6,13 @@ namespace OpenInApp.Command
     {
         public string ActualPathToExe { get; set; }
         public string Caption { get; set; }
-        public string ExecutableFileToBrowseFor { get; set; }
+        public string ExecutableFileToBrowseFor { get; set; } //gregt is this used ? rename to KeyToExecutable ?
         public string FileQuantityWarningLimit { get; set; }
         public bool IsFromSolutionExplorer { get; set; }
+        public bool SeparateProcessPerFileToBeOpened { get; set; }
         public IServiceProvider ServiceProvider { get; set; }        
         public bool SuppressTypicalFileExtensionsWarning { get; set; }
         public string TypicalFileExtensions { get; set; }
-
-
-        public bool SeparateProcessPerFileToBeOpened{ get; set; }
         public bool UseShellExecute { get; set; }
     }
 }
