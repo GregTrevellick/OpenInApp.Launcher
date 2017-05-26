3ae30f8d1d0f634b257027e43267ac2215b1ca79
GregTrevellick <gtrevellick@googlemail.com>
2017-05-26 22:17:52 +0100

    open in treesize free ==> open path in treesize free

--- src/OpenInTreeSizeFree/source.extension.cs
+++ src/OpenInTreeSizeFree/source.extension.cs
@@ -8,8 +8,8 @@ namespace OpenInTreeSizeFree
     static class Vsix
     {
         public const string Id = "0597a0d5-0bfc-44a5-924c-b556c5047018";
-        public const string Name = "Open in TreeSize Free";
-        public const string Description = @"Open multiple files simultaneously in TreeSize Free directly from Solution Explorer or Code Editor window.";
+        public const string Name = "Open path in TreeSize Free";
+        public const string Description = @"Open multiple file paths simultaneously in TreeSize Free directly from Solution Explorer or Code Editor window.";
         public const string Language = "en-US";
         public const string Version = "1.1.11";
         public const string Author = "GregTrevellick";
