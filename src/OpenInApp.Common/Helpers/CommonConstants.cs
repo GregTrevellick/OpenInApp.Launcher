c89a3a26adb4b5affbafd0a6944ff5c4157994ac
GregTrevellick <gtrevellick@googlemail.com>
2017-06-01 08:10:19 +0100

    rename "file" in quantity limit messages to "artefact"

--- src/OpenInApp.Common/Helpers/CommonConstants.cs
+++ src/OpenInApp.Common/Helpers/CommonConstants.cs
@@ -10,13 +10,13 @@ namespace OpenInApp.Common.Helpers
         public const string ActualPathToExeOptionDetailedDescription = "Specify the absolute install path for the application.";
         public const string ActualPathToExeOptionLabelPrefix = "Application path to ";
         public const string CategorySubLevel = "General";
-        public static string ConfirmOpenFileQuantityExceedsWarningLimit = @"You have selected a large quantity of files to be opened.";
+        public static string ConfirmOpenFileQuantityExceedsWarningLimit = @"You have selected a large quantity of artefacts to be opened.";
         public const string ConfirmOpenNonTypicalFile = "One or more selected files typically contains data that the application does not expect.";
         public static string ContinueAnyway = "Click OK to open anyway, or CANCEL to return to Visual Studio.";
         public const string DefaultFileQuantityWarningLimit = "10";
         public static string FileQuantityWarningLimitInvalid = "Invalid integer value specified for:" + Environment.NewLine + Environment.NewLine + FileQuantityWarningLimitOptionLabel;
-        public const string FileQuantityWarningLimitOptionDetailedDescription = "The number of files that can be opened at one time before a warning is displayed. You will be able to open files whose count exceeds this number, but you will be informed that the number of files is very high. This allows you to avoid accidentely opening hundreds or thousands of files which may affect performance of your machine.";
-        public const string FileQuantityWarningLimitOptionLabel = "Simultaneous file opening count warning limit";
+        public const string FileQuantityWarningLimitOptionDetailedDescription = "The number of artefacts that can be opened at one time before a warning is displayed. You will be able to open artefacts whose count exceeds this number, but you will be informed that the number of artefacts is very high. This allows you to avoid accidentely opening hundreds or thousands of files artefacts may affect performance of your machine.";
+        public const string FileQuantityWarningLimitOptionLabel = "Simultaneous artefacts opening count warning limit";
      
         public static string InformUserMissingFile(string missingFileName)
         {
