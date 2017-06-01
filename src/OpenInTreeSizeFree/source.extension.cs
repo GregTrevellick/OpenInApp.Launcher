9627f89984fac34bc7bc79916bae9dd45173a120
GregTrevellick <gtrevellick@googlemail.com>
2017-06-01 08:07:25 +0100

    minor product name/desc updates

--- src/OpenInTreeSizeFree/source.extension.cs
+++ src/OpenInTreeSizeFree/source.extension.cs
@@ -8,7 +8,7 @@ namespace OpenInTreeSizeFree
     static class Vsix
     {
         public const string Id = "0597a0d5-0bfc-44a5-924c-b556c5047018";
-        public const string Name = "Open folder in TreeSize Free";
+        public const string Name = "TreeSize Free";
         public const string Description = @"Open multiple folders simultaneously in TreeSize Free directly from Solution Explorer or Code Editor window.";
         public const string Language = "en-US";
         public const string Version = "1.1.14";
--- src/OpenInTreeSizeProfessional/source.extension.cs
+++ src/OpenInTreeSizeProfessional/source.extension.cs
@@ -8,10 +8,10 @@ namespace OpenInTreeSizeProfessional
     static class Vsix
     {
         public const string Id = "a1b88397-2308-43ac-a471-6c1936720a6f";
-        public const string Name = "Open folder in TreeSize Professional";
+        public const string Name = "TreeSize Professional";
         public const string Description = @"Open multiple folders simultaneously in TreeSize Professional directly from Solution Explorer or Code Editor window.";
         public const string Language = "en-US";
-        public const string Version = "1.1.13";
+        public const string Version = "1.1.14";
         public const string Author = "GregTrevellick";
         public const string Tags = "open, open in, TreeSize, TreeSize Professional, TreeSizeProfessional, disc space";
     }
