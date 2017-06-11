1e074839372987a9980cc31799b030bca227a0bd
GregTrevellick <gtrevellick@googlemail.com>
2017-06-11 21:42:32 +0100

    more exclusions

--- src/OpenInAbracadabra/Helpers/ConstantsForApp.cs
+++
@@ -1,9 +0,0 @@
-﻿//using OpenInApp.Common.Helpers;
-
-//namespace OpenInAbracadabra.Helpers
-//{
-//    public class ConstantsForApp 
-//    {
-//    //    internal const string CommonActualPathToExeOptionLabel = CommonConstants.ActualPathToExeOptionLabelPrefix + KeyToExecutableString.Abracadabra;
-//    }
-//}
