9cd63ee8a97fb3cf0a54f51a8e37b09939e2e9a6
Greg Trevellick <gtrevellick@googlemail.com>
2017-06-22 13:35:05 +0100

    simplify codebase + IDM_VS_MENU_PROJECT ==> IDM_VS_CTXT_PROJNODE for WinDirStat

--- src/OpenInWinDirStat/VSCommandTable_OpenInApp.cs
+++ src/OpenInWinDirStat/VSCommandTable_OpenInApp.cs
@@ -1,11 +1,11 @@
-﻿namespace OpenInWinDirStat
-{
-    /// <summary>
-    /// Helper class to facilitate a vsix sharing code that handles command placement for IDM_VS_CTXT_FOLDERNODE, even though this vsix intentionally does not have IDM_VS_CTXT_FOLDERNODE
-    /// </summary>
-    internal sealed partial class PackageIds
-    {
-        //public const int CmdIdOpenInAppFolderNode = int.MinValue;
-        //public const int CmdIdOpenInAppProject = int.MinValue;
-    }
-}
+﻿//namespace OpenInWinDirStat
+//{
+//    /// <summary>
+//    /// Helper class to facilitate a vsix sharing code that handles command placement for IDM_VS_CTXT_FOLDERNOD, even though this vsix intentionally does not have IDM_VS_CTXT_FOLDERNOD
+//    /// </summary>
+//    internal sealed partial class PackageIds
+//    {
+//        //public const int CmdIdOpenInAppFolderNode = int.MinValue;
+//        //public const int CmdIdOpenInAppProject = int.MinValue;
+//    }
+//}
