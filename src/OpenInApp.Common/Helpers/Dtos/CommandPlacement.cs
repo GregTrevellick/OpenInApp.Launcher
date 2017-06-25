1af3026e7e0df9a4b1ce3a556bd441f2373c1e42
Greg Trevellick <gtrevellick@googlemail.com>
2017-06-25 01:05:04 +0100

    comments

--- src/OpenInApp.Common/Helpers/Dtos/CommandPlacement.cs
+++ src/OpenInApp.Common/Helpers/Dtos/CommandPlacement.cs
@@ -9,5 +9,8 @@
         IDM_VS_CTXT_FOLDERNODE,//Folder selected
         IDM_VS_CTXT_ITEMNODE,//File selected
         IDM_VS_CTXT_PROJNODE,//Project selected
+        //IDM_VS_CTXT_XPROJ_MULTIPROJ,//applies when the current selection consists of multiple root project nodes only. 
+        //IDM_VS_CTXT_XPROJ_PROJITEM,//applies when the current selection contains a mix of root project node(s) and project item(s). In addition, the selection may or may not contain the solution node. 
+        //IDM_VS_CTXT_XPROJ_MULTIITEM,//applies when the current selection contains project items from multiple projects within the solution, or when items of different types are selected within the same project. 
     }
 }
