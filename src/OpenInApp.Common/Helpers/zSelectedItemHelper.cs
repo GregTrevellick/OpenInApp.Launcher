25511b5401fb43ca9edd82efd104f98397fda834
Greg Trevellick <gtrevellick@googlemail.com>
2017-07-05 06:40:13 +0100

    delete dead code

---
+++ src/OpenInApp.Common/Helpers/zSelectedItemHelper.cs
@@ -0,0 +1,12 @@
+﻿//using EnvDTE;
+
+//namespace OpenInApp.Common.Helpers
+//{
+//    public class SelectedItemHelper
+//    {
+//        public static string GetFolderSelectedFullPath(SelectedItem selectedItem)
+//        {
+//            return selectedItem.ProjectItem.FileNames[0];
+//        }
+//    }
+//}
