56e408779271401fb8776c20037795dbda592487
Greg Trevellick <gtrevellick@googlemail.com>
2017-07-07 08:05:26 +0100

    added open cover

---
+++ src/OpenInApp.Common.Tests/_RunCodeCoverageInOutput.bat
@@ -0,0 +1,8 @@
+﻿..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe
+
+"..\..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -target:"..\..\..\packages\NUnit.Runners.2.6.3\tools\nunit-console.exe" -targetargs:"/nologo OpenInApp.Common.Tests.dll /noshadow" -filter:"+[OpenInApp.Common.Tests]OpenInApp.Common.Tests*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"_CodeCoverageResult7.xml"
+
+
+
+"..\..\..\packages\ReportGenerator.2.5.9\tools\ReportGenerator.exe" "-reports:_CodeCoverageResult7.xml" "-targetdir:_CodeCoverageReport7"
+REM target:"..\..\..\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe"
---
+++ src/OpenInApp.Common.Tests/zRunNunitTests_CopyToOutputDirAlways.bat
@@ -0,0 +1,5 @@
+﻿REM ..\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe OpenInApp.Common.Tests.dll /noshadow -output:"_CodeCoverageResult.xml"
+
+
+
+
