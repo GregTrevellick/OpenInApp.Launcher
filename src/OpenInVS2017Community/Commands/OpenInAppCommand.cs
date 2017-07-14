046ab706f0a2caac7d89bf6bdd55150e10b69347
Greg Trevellick <gtrevellick@googlemail.com>
2017-07-14 08:10:21 +0100

    add proj & folder node to vs2017 comm.

--- src/OpenInVS2017Community/Commands/OpenInAppCommand.cs
+++ src/OpenInVS2017Community/Commands/OpenInAppCommand.cs
@@ -24,6 +24,8 @@ namespace OpenInVS2017Community.Commands
                 PackageGuids.guidOpenInVsCmdSetString,
                 PackageIds.CmdIdOpenInAppItemNode,
                 PackageIds.CmdIdOpenInAppCodeWin,
+                PackageIds.CmdIdOpenInAppFolderNode,
+                PackageIds.CmdIdOpenInAppProjNode,
                 GeneralOptions.keyToExecutableEnum,
                 VSPackage.Options.ActualPathToExe,
                 VSPackage.Options.FileQuantityWarningLimit,
