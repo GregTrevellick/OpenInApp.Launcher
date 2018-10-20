331d266d54383e37b7d4ca66919242a000717b21
Greg Trevellick <gtrevellick@googlemail.com>
2018-10-20 12:12:32 +0100

    covert to async packages

--- src/OpenInMSPaint/VSPackage.cs
+++ src/OpenInMSPaint/VSPackage.cs
@@ -5,25 +5,28 @@ using OpenInMSPaint.Options.MSPaint;
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace OpenInMSPaint
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration(productName: "#110", productDetails: "#112", productId: Vsix.Version, IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(PackageGuids.guidOpenInAppPackageString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
     [ProvideOptionPage(typeof(GeneralOptions), Vsix.Name, CommonConstants.CategorySubLevel, 0, 0, true)]
-    public sealed class VSPackage : Package
+    public sealed class VSPackage : AsyncPackage
     {
         public static GeneralOptions Options { get; private set; }
 
-        protected override void Initialize()
+        protected override async System.Threading.Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
+            await JoinableTaskFactory.SwitchToMainThreadAsync();
             Options = (GeneralOptions)GetDialogPage(typeof(GeneralOptions));
 
             new OpenInAppCommand(this).Initialize();
-            base.Initialize();
+
         }
     }
 }
