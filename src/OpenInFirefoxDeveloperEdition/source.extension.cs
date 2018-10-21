d3c427b40a2daabaa08e80c0fea8a1ec65402559
Greg Trevellick <gtrevellick@googlemail.com>
2018-10-21 12:40:43 +0100

    .

--- src/OpenInFirefoxDeveloperEdition/source.extension.cs
+++ src/OpenInFirefoxDeveloperEdition/source.extension.cs
@@ -11,8 +11,8 @@ namespace OpenInFirefoxDeveloperEdition
         public const string Name = "Open in Firefox Developer Edition";
         public const string Description = @"Open multiple files simultaneously in Firefox Developer Edition directly from Solution Explorer or Code Editor window.";
         public const string Language = "en-US";
-        public const string Version = "1.1.51";
+        public const string Version = "1.1.52";
         public const string Author = "GregTrevellick";
-        public const string Tags = "open, open in, Firefox Developer Edition, Firefox, Mozilla";
+        public const string Tags = "Firefox Developer Edition, Firefox, Mozilla";
     }
 }
